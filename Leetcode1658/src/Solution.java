b52167f9e6498fdf01b4bc6033c067938ac09974
Mxln <13924291049@163.com>
2023-01-25 18:26:28 +0800

    1658(76/94)

--- Leetcode1658/src/Solution.java
+++ Leetcode1658/src/Solution.java
@@ -2,63 +2,67 @@ import java.util.Scanner;
 
 public class Solution {
     public static void main(String[] args) {
-        int[] nums = {1, 1, 2, 3, 4};
-        int x = 5;
+        int[] nums = {3,2,20,1,1,3};
+        int x = 10;
         Solution solution = new Solution();
-        solution.minOperations(nums, x);
+
+        System.out.println(solution.minOperations(nums, x));
     }
 
     public int minOperations(int[] nums, int x) {
-        int[][] result = new int[nums.length + 1][nums.length + 1];
-        int[][] operatetime = new int[nums.length + 1][nums.length + 1];
+        int[][] result = new int[nums.length][nums.length];
+        int answer = Integer.MAX_VALUE;
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result.length; j++) {
-                result[i][j]=Integer.MAX_VALUE;
+                result[i][j] = 0;
             }
         }
-        for (int i = 0; i < result.length; i++) {
-            result[0][i] = x;
-            result[i][0] = x;
-        }
-        for (int i = 0; i < operatetime.length; i++) {
-            operatetime[0][i] = 0;
-            operatetime[i][0] = 0;
-        }
-        putresult(result,operatetime);
-        for (int i = 1; i < nums.length + 1; i++) {
-            for (int j = 1; j < nums.length + 1; j++) {
-                //处理表格左值
-                int leftopt = operatetime[i][j-1];
-                int rnum = nums[nums.length-(i+j-2-leftopt)];
-                if (rnum<result[i][j-1]){
-                    result[i][j]=result[i][j-1]-rnum;
-                    operatetime[i][j]=operatetime[i][j];
+        result[0][0] = x;
+        //putresult(result);
+        for (int i = 0; i < nums.length; i++) {
+            if (i != 0) {
+                int lnum = nums[i - 1];
+                if (result[i - 1][0] >= lnum) {
+                    result[i][0] = result[i - 1][0] - lnum;
+                } else {
+                    result[i][0] = -1;
+                    break;
                 }
-                //处理表格上值
-                int upopt = operatetime[i-1][j];
-                int lnum = nums[upopt];
-                if (lnum<result[i][j-1]&result[i][j-1]-lnum<result[i][j]){
-                    result[i][j]=result[i][j-1]-lnum;
-                    operatetime[i][j]=operatetime[i][j]+1;
+            }
+            if (result[i][0] == 0) {
+                if (i + 0 < answer) {
+                    answer = i + 0;
+                }
+            }
+            for (int j = 1; j < nums.length; j++) {
+                int rnum = nums[nums.length - j];
+                if (result[i][j - 1] >= rnum) {
+                    result[i][j] = result[i][j - 1] - rnum;
+                } else {
+                    result[i][j] = -1;
+                    break;
+                }
+                if (result[i][j] == 0) {
+                    if (i + j < answer) {
+                        answer = i + j;
+                    }
                 }
             }
         }
-        return 1;
+        //putresult(result);
+        if (answer== Integer.MAX_VALUE){
+            answer=-1;
+        }
+        return answer;
     }
 
-    public void putresult(int[][] result, int[][] operatetime) {
+    public void putresult(int[][] result) {
         for (int i = 0; i < result.length; i++) {
             for (int j = 0; j < result.length; j++) {
                 System.out.printf("%d ", result[i][j]);
             }
             System.out.println();
         }
-        System.out.println();
-        for (int i = 0; i < operatetime.length; i++) {
-            for (int j = 0; j < operatetime.length; j++) {
-                System.out.printf("%d ", operatetime[i][j]);
-            }
-            System.out.println();
-        }
+
     }
 }
