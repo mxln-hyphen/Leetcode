84738fe26b4a677293847428e1398358f1a70448
Mxln <13924291049@163.com>
2023-01-26 12:55:09 +0800

    (94/94)

--- LeetCode1663/src/Solution.java
+++ LeetCode1663/src/Solution.java
@@ -1,33 +1,32 @@
 class Solution {
     public static void main(String[] args) {
-        Solution solution=new Solution();
-        System.out.println(solution.getSmallestString(91146,181802));
+        Solution solution = new Solution();
+        System.out.println(solution.getSmallestString(91146, 181802));
     }
+
     public String getSmallestString(int n, int k) {
         String answer = "";
         int znum = k / 26;
         n = n - znum;
         k = k % 26;
         int anum = 0;
-        int time=0;
         while (n > k) {
             k += 26;
             n += 1;
-            znum-=1;
+            znum -= 1;
         }
         anum += n - 1;
         k -= n - 1;
-        for (int i = 0; i < anum; i++) {
-            answer += "a";
+        if(anum>0){
+            answer+="a".repeat(anum);
         }
-        if(k!=0&n!=0) {
+        if (k != 0 & n != 0) {
             String ch = "" + (char) (k + 96);
             answer += ch;
         }
-        for (int i = 0; i < znum; i++) {
-            answer += "z";
+        if(znum>0) {
+            answer += "z".repeat(znum);
         }
-        System.out.println(time);
         return answer;
     }
 }
