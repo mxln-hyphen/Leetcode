d6cc43e93c12abd3e3dca4ccf78c9e16a266c0e0
Mxln <13924291049@163.com>
2023-02-04 15:18:23 +0800

    NF

---
+++ LeetCode OfferⅡ 015/src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.findAnagrams);
+    }
+
+    public List<Integer> findAnagrams(String s, String p) {
+
+    }
+}
