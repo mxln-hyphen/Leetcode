9661e51e349c4ffad1ea199ef27abade6dd3d148
Mxln <13924291049@163.com>
2023-02-21 19:18:13 +0800

    (215/215)T:80.28 S:54.07

---
+++ LeetCode OfferⅡ 046/src/Solution.java
@@ -0,0 +1,32 @@
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        if(root==null){
+            return new ArrayList<Integer>();
+        }
+        List<Integer> res = new ArrayList<Integer>();
+        Queue<TreeNode> queue = new ArrayDeque<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            while (len > 0) {
+                len--;
+                TreeNode t = queue.poll();
+                if (t.left != null) {
+                    queue.offer(t.left);
+                }
+                if (t.right != null) {
+                    queue.offer(t.right);
+                }
+                if (len == 0) {
+                    res.add(t.val);
+                }
+            }
+        }
+        return res;
+    }
+}
