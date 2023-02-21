19de2477f477e4c5e2349b71ee52548331e6d1b7
Mxln <13924291049@163.com>
2023-02-21 18:57:01 +0800

    (78/78)T:83.28 S:6.07

--- LeetCode OfferⅡ 044/src/Solution.java
+++ LeetCode OfferⅡ 044/src/Solution.java
@@ -5,31 +5,41 @@ import java.util.Queue;
 
 class Solution {
     public static void main(String[] args) {
+        TreeNode root = new TreeNode().Createtree("1,3,2,5,3,null,9");
+
         Solution solution = new Solution();
-        System.out.println(solution.largestValues());
+        System.out.println(solution.largestValues(root));
     }
 
     public List<Integer> largestValues(TreeNode root) {
         List<Integer> answer = new ArrayList<>();
+        if (root == null) {
+            return answer;
+        }
         Queue<TreeNode> queue = new ArrayDeque<>();
         TreeNode p = root;
         int layernum = 1;
-        int layermax = -1;
-        int layersize = 1;
+        int layermax = Integer.MIN_VALUE;
+        queue.add(root);
         while (!queue.isEmpty()) {
+            p = queue.poll();
             if (p != null) {
-                queue.add(p.left);
-                queue.add(p.right);
+                if (p.left != null) {
+                    queue.add(p.left);
+                }
+                if (p.right != null) {
+                    queue.add(p.right);
+                }
                 layermax = p.val > layermax ? p.val : layermax;
+            } else {
+
             }
             layernum--;
             if (layernum == 0) {
                 answer.add(layermax);
-                layermax = -1;
-                layersize *= 2;
-                layernum = layersize;
+                layermax = Integer.MIN_VALUE;
+                layernum = queue.size();
             }
-            p = queue.poll();
         }
         return answer;
     }
--- LeetCode OfferⅡ 044/src/TreeNode.java
+++ LeetCode OfferⅡ 044/src/TreeNode.java
@@ -21,23 +21,35 @@ public class TreeNode {
         this.right = right;
     }
 
-    TreeNode(String val) {
-        if (val != "null") {
-            this.val = Integer.parseInt(val);
+    public TreeNode NewNode(String val) {
+        if (!val.equals("null")) {
+            return new TreeNode(Integer.parseInt(val));
+        }else{
+            return null;
         }
     }
 
-
-    public TreeNode Createtree(String[] args) {
+    public TreeNode Createtree(String str) {
+        String[] args = str.split(",");
         if (args.length == 0) {
             return null;
         }
         Queue<TreeNode> queue = new ArrayDeque<>();
-        TreeNode root = new TreeNode(args[0]);
+        TreeNode root = NewNode(args[0]);
         TreeNode p = root;
-        for (String arg : args) {
-            if (p.left==null)
+        int i = 1;
+        while (i != args.length) {
+            p.left = NewNode(args[i++]);
+            if(p.left!=null)
+                queue.add(p.left);
+            if (i != args.length) {
+                p.right = NewNode(args[i++]);
+                if(p.left!=null)
+                    queue.add(p.right);
+            }
+            p = queue.poll();
         }
+        return root;
     }
 
 }
