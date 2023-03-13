d70d125868702de9d1153417df4f75fda8c0a234
Mxln <13924291049@163.com>
2023-03-13 18:44:16 +0800

    (106/106) T:30.74 S:80.29

--- LeetCode OfferⅡ 096/src/Solution.java
+++ LeetCode OfferⅡ 096/src/Solution.java
@@ -12,30 +12,29 @@ class Solution {
         if (s1.length() + s2.length() != s3.length())
             return false;
 
-        boolean[][] dp = new boolean[2][s2.length() + 1];
+        boolean[] dp = new boolean[s2.length() + 1];
 
-        dp[0][0] = true;
+        dp[0] = true;
 
         for (int j = 0; j < s2.length(); j++) {
-            dp[0][j + 1] = (s2.charAt(j) == s3.charAt(j)) && dp[0][j];
+            dp[j + 1] = (s2.charAt(j) == s3.charAt(j)) && dp[j];
         }
 
         int i;
 
         for (i = 0; i < s1.length(); i++) {
-            dp[(i + 1) % 2][0] = (s1.charAt(i) == s3.charAt(i)) && dp[i % 2][0];
+            boolean prev = dp[0];
+            dp[0] = (s1.charAt(i) == s3.charAt(i)) && dp[0];
             for (int j = 0; j < s2.length(); j++) {
                 char ch1 = s1.charAt(i);
                 char ch2 = s2.charAt(j);
                 char ch3 = s3.charAt(i + j + 1);
 
-                dp[(i + 1) % 2][j + 1] = (ch1 == ch3) && (dp[i % 2][j + 1]) || (ch2 == ch3) && (dp[(i + 1) % 2][j]);
+                dp[j + 1] = (ch1 == ch3) && (dp[j + 1]) || (ch2 == ch3) && (dp[j]);
+                prev = dp[j + 1];
 
             }
         }
-
-        int lasti = s1.length() % 2;
-
-        return dp[(lasti][s2.length()];
+        return dp[s2.length()];
     }
 }
