c0cd36d7dc0055c8c9f0309554b15964fe936efe
Mxln <13924291049@163.com>
2023-04-10 16:58:03 +0800

    (76/76) T:84.19 S:83.58

---
+++ LeetCode 327/src/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.methodname);
+    }
+
+    public int countRangeSum(int[] nums, int lower, int upper) {
+
+    }
+}
