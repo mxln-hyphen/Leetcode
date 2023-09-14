d2ce998a52c0be63ceb20d7a21d167cbd5cde9dc
Mxln <13924291049@163.com>
2023-09-14 18:51:53 +0800

    二分查找，把问题转化为偷取最大金额为y时，最多能偷取的房子数x，如果x>k，二分查找继续去找下一个更小的y。

---
+++ LeetCode 2560/src/Solution.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+class Solution {
+    public static void main(String[] args) {
+        int[] nums = new int[]{2,7,9,3,1};
+        Solution solution = new Solution();
+        System.out.println(solution.minCapability(nums,2));
+    }
+
+
+    public int minCapability(int[] nums, int k) {
+        int lower = Arrays.stream(nums).min().getAsInt();
+        int upper = Arrays.stream(nums).max().getAsInt();
+        while (lower <= upper) {
+            int middle = (lower + upper) / 2;
+            int count = 0;
+            boolean visited = false;
+            for (int x : nums) {
+                if (x <= middle && !visited) {
+                    count++;
+                    visited = true;
+                } else {
+                    visited = false;
+                }
+            }
+            if (count >= k) {
+                upper = middle - 1;
+            } else {
+                lower = middle + 1;
+            }
+        }
+        return lower;
+    }
+
+}
